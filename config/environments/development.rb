1998bea05687ea571baf2657ff25c1b68b783d3e
EC2 Default User <shinya.sin09@gmail.com>
2022-08-07 14:20:31 +0000

    kaishi

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "430290f35854436896feb7dee1d85db5.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end

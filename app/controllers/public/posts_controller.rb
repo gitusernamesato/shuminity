059cc41ae697abdb310a52f8a4afc9bd83001e1c
EC2 Default User <shinya.sin09@gmail.com>
2022-08-13 11:50:56 +0000

    [add]newpost

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,31 @@
 class Public::PostsController < ApplicationController
+  
+
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
   end
 
   def index
+    @post = Post.new
+    @posts = Post.all
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+      redirect_to posts_path
+    # else
+    #   @posts = Post.all
+    #   render :index
+    # end
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 end

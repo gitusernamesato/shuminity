02e284c6e48b4acf2e91c1262ac50fdb52682fac
EC2 Default User <shinya.sin09@gmail.com>
2022-08-16 14:17:08 +0000

    [add]create_tags

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -4,6 +4,7 @@ class Public::PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
+    @post_tags = @post.tags
   end
 
   def edit
@@ -12,21 +13,44 @@ class Public::PostsController < ApplicationController
   def index
     @post = Post.new
     @posts = Post.all
+    @tag_list = Tag.all
   end
   
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    @post.save
-      redirect_to posts_path
-    # else
-    #   @posts = Post.all
-    #   render :index
-    # end
+    tag_list = params[:post][:tag].split(',')
+    if @post.save
+     @post.save_tag(tag_list)
+     redirect_to posts_path
+    else
+     @posts = Post.all
+     render :index
+    end
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+    @tag_list=@post.tags.pluck(:tag).join(',')
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    tag_list=params[:post][:tag].split(',')
+    if @post.update(post_params)
+       @post.save_tag(tag_list)
+       redirect_to post_path(@post.id),notice:'投稿完了しました:)'
+    else
+      render:edit
+    end
   end
   
   private
   def post_params
     params.require(:post).permit(:title, :body, :image)
+  end
+  
+  def tag_params
+    params.require(:tag).permit(:tag)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  has_many :post_tags, dependent: :destroy
+  has_many :tags,through: :post_tags
+   
   has_one_attached :image
   
   # image_tag post.getimage(w,h)で投稿画像の読み込み、サイズ調整
@@ -19,4 +21,24 @@ class Post < ApplicationRecord
     favorites.exists?(user_id: user.id)
   end
   
+   
+  def save_tag(sent_tags)
+    # タグが存在していれば、タグの名前を配列として全て取得
+    current_tags = self.tags.pluck(:tag) unless self.tags.nil?
+    # 現在取得したタグから送られてきたタグを除いてoldtagとする
+    old_tags = current_tags - sent_tags
+    # 送信されてきたタグから現在存在するタグを除いたタグをnewとする
+    new_tags = sent_tags - current_tags
+    
+    # 古いタグを消す
+    old_tags.each do |old|
+      self.tags.delete　Tag.find_by(tag: old)
+    end
+    
+    # 新しいタグを保存
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(tag: new)
+      self.tags << new_post_tag
+   end
+  end
 end

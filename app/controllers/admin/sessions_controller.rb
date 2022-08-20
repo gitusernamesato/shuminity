b051423a57aeb533250ce97442598bb0d5bf0748
EC2 Default User <shinya.sin09@gmail.com>
2022-08-20 02:11:06 +0000

    admins

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
-  def after_sign_out_path_for(resource)
-    admin_session_path
-  end
+  # def after_sign_out_path_for(resource)
+  #   admin_session_path
+  # end
   
   # before_action :configure_sign_in_params, only: [:create]
 

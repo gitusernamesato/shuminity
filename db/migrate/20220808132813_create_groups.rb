c728a597c5a1a0060ba888e4f51673139328e790
EC2 Default User <shinya.sin09@gmail.com>
2022-08-25 13:11:44 +0000

    deploy

--- db/migrate/20220808132813_create_groups.rb
+++ db/migrate/20220808132813_create_groups.rb
@@ -3,7 +3,7 @@ class CreateGroups < ActiveRecord::Migration[6.1]
     create_table :groups do |t|
       t.string :group_name, null: false, default: ""
       t.text :introduction, null: false#, default: ""
-      t.integer :owner_id, null: false, default: ""
+      t.integer :owner_id, null: false#, default: ""
       
       t.timestamps
     end

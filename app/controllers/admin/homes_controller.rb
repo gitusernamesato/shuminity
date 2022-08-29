e633990165b1d85fb9167fffb3e9d83ef47e8702
EC2 Default User <shinya.sin09@gmail.com>
2022-08-29 07:47:19 +0000

    [update]admin_layouts

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top
-    @posts = Post.page(params[:page])
+    @posts = Post.page(params[:page]).reverse_order.per(5)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(5)
     @groups = @user.groups
   end
 

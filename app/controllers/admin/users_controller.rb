f2797785bc53a9bb92f99cf5c984ad2db89866e2
EC2 Default User <shinya.sin09@gmail.com>
2022-09-05 08:15:10 +0000

    [fix]admin_user_edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,20 +8,23 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
-  
+
   def edit
     @user = User.find(params[:id])
+  end
 
   def update
     @user = User.find(params[:id])
     @posts = @user.posts.all
     if @user.update(user_params)
-        if user_params[:is_deleted] ==  true
+       if @user.is_deleted == true
+          # Post.where([user_id: @user.id]).update_all(is_hidden:true)
           @posts.update_all(is_hidden: true)
-        else
-          @posts.update_all(is_hidden: false)
-        end
-      redirect_to admin_user_path(@user)
+          redirect_to admin_user_path(@user)
+       elsif @user.is_deleted == false
+          # @posts.update_all(is_hidden: false) 
+          redirect_to admin_users_path
+       end
     else
       redirect_to admin_user_path(@user)
     end

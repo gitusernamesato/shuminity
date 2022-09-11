3b6bd1848e4879934ee9600191dfb9621ac821e9
EC2 Default User <shinya.sin09@gmail.com>
2022-09-11 14:21:17 +0000

    [fix]user_withdraw

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :reject_user, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -20,8 +21,19 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource_or_scope)
     root_path
   end
-  # protected
 
+  protected
+
+  def reject_user
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user
+      if (@user.valid_password?(params[:user][:password]) && (@user.is_deleted == true))
+        flash[:error] = "退会済みです。"
+        redirect_to new_user_session_path
+        # redirect_to root_path
+      end
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])

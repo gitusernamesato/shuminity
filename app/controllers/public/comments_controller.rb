109577e7c1bdfc21ce730472cffffa66ae9cd966
EC2 Default User <shinya.sin09@gmail.com>
2022-10-09 18:21:02 +0900

    [fin]ajax_comments

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,21 +1,22 @@
 class Public::CommentsController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
     comment = current_user.comments.new(comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
-  
+
   def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
     Comment.find(params[:id]).destroy
-    redirect_to post_path(params[:post_id])
   end
-  
+
   private
 
   def comment_params
     params.require(:comment).permit(:comment)
   end
-  
+
 end
